a44bbcf660a803c9c7323cf43165f7c415d802ea
Muhammad Bin Ali <muhammadbinali706@gmail.com>
2024-07-21 20:39:04 -0400

    Make manager singleton

--- Manager/Manager.h
+++ Manager/Manager.h
@@ -1,13 +1,28 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
+#include <memory>
+
 #include "PlayerType.h"
 
+// class Manager {
+//    public:
+//     static void setupGame();
+//     static void startGame(PlayerType::Type White, PlayerType::Type Black);
+//     static void closeGame();
+// };
+
 class Manager {
+    Manager() {}
+
    public:
-    void setupGame();
-    void startGame(PlayerType::Type White, PlayerType::Type Black);
-    void closeGame();
+    Manager(Manager const&) = delete;
+    Manager& operator=(Manager const&) = delete;
+
+    static std::shared_ptr<Manager> instance() {
+        static std::shared_ptr<Manager> s{new Manager};
+        return s;
+    }
 };
 
 #endif
