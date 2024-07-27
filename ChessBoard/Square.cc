28153100a03e6c690bd63636dbe0a7aa3bfbb461
l4ni <nixiaotang2004@gmail.com>
2024-07-27 19:31:40 -0400

    remove debug

--- ChessBoard/Square.cc
+++ ChessBoard/Square.cc
@@ -14,8 +14,6 @@ Position Square::getPosition() const { return _position; }
 void Square::setPiece(std::shared_ptr<Piece> piece) {
     _piece = piece;
     if (!_piece.expired()) piece->getPlayer()->addAlivePiece(piece);
-        std::cout<< "C" << std::endl;
-
 }
 
 void Square::disconnectPiece() {
