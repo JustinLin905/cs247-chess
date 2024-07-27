c2f2000d9ee329c1097c162c9ba6f8b50e4af4b9
mb2ali <mb2ali@uwaterloo.ca>
2024-07-26 21:35:30 -0400

    Replace refs with smart ptrs

--- Game/Game.cc
+++ Game/Game.cc
@@ -1,14 +1,15 @@
 #include "Game.h"
 
 #include <iostream>
+#include <memory>
 
 #include "../Move/Move.h"
 #include "../Player/HumanPlayer.h"
 #include "../PlayerType/PlayerType.h"
 
 void Game::initPlayers(PlayerType::Type white, PlayerType::Type black) {
-    if (white == PlayerType::Type::HUMAN) _white = HumanPlayer(Color::WHITE);
-    if (black == PlayerType::Type::HUMAN) _black = HumanPlayer(Color::BLACK);
+    if (white == PlayerType::Type::HUMAN) _white = std::unique_ptr<HumanPlayer>(new HumanPlayer(Color::WHITE, *this));
+    if (black == PlayerType::Type::HUMAN) _black = std::unique_ptr<HumanPlayer>(new HumanPlayer(Color::BLACK, *this));
 }
 
 bool Game::makeTurn(Move move) {
--- Game/Game.h
+++ Game/Game.h
@@ -1,23 +1,25 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <memory>
+
 #include "../ChessBoard/ChessBoard.h"
 #include "../Move/Move.h"
 #include "../Player/Player.h"
 #include "../PlayerType/PlayerType.h"
 
 class Game {
-    ChessBoard& _board;
-    Player& _white;
-    Player& _black;
+    ChessBoard _board;
+    std::unique_ptr<Player> _white;
+    std::unique_ptr<Player> _black;
 
    public:
     void initPlayers(PlayerType::Type white, PlayerType::Type black);
     bool makeTurn(Move move);
     void reverseMoves(int n);
 
-    Player& getWhite() { return _white; }
-    Player& getBlack() { return _black; }
+    std::unique_ptr<Player> getWhite() const;
+    std::unique_ptr<Player> getBlack() const;
 };
 
 #endif
