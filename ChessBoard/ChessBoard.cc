779ec0508f3735c1d395b5f3aeb56874909b89b8
mb2ali <mb2ali@uwaterloo.ca>
2024-07-28 15:34:37 -0400

    oops

--- ChessBoard/ChessBoard.cc
+++ ChessBoard/ChessBoard.cc
@@ -32,7 +32,7 @@ void ChessBoard::defaultSetup(std::unique_ptr<Player> &whitePlayer, std::unique_
         {'-', '-', '-', '-', '-', '-', '-', '-'},
         {'-', '-', '-', '-', '-', '-', '-', '-'},
         {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', '-', 'K', '-', 'K', '-', '-', 'R'}};
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
