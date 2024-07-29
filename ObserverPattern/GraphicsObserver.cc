7b672f0f53d938ab32645c98f75d5c24375372b6
Justin Lin <jk3lin@uwaterloo.ca>
2024-07-29 16:46:00 -0400

    Graphics observer coordinates

--- ObserverPattern/GraphicsObserver.cc
+++ ObserverPattern/GraphicsObserver.cc
@@ -3,20 +3,28 @@
 #include "../ChessBoard/ChessBoard.h"
 
 GraphicsObserver::GraphicsObserver(std::weak_ptr<ChessBoard> chess_board) : _chess_board(chess_board) {
-    _w = new Xwindow(800, 800);
+    _w = new Xwindow(900, 900);
     _chess_board.lock()->attach(this);
 }
 
 GraphicsObserver::~GraphicsObserver() { delete _w; }
 
 void GraphicsObserver::notify() {
+    // Render top coordinate row
+    for (int i = 1; i < 9; ++i) {
+        _w->fillRectangle(100 * i, 0, 100, 100, Xwindow::Green);
+        _w->drawString(100 * i + 50, 50, std::string{char(64 + i)});
+    }
+
     for (int i = 0; i < 8; ++i) {
+        _w->fillRectangle(0, 100 * (i + 1), 100, 100, Xwindow::Green);
+        _w->drawString(50, 100 * (i + 1) + 50, std::to_string(8 - i));
         for (int j = 0; j < 8; ++j) {
             bool isWhite = (i + j) % 2 == 0;
             if (isWhite) {
-                _w->fillRectangle(100 * j, 100 * i, 100, 100, Xwindow::White);
+                _w->fillRectangle(100 * (j + 1), 100 * (i + 1), 100, 100, Xwindow::White);
             } else {
-                _w->fillRectangle(100 * j, 100 * i, 100, 100, Xwindow::Black);
+                _w->fillRectangle(100 * (j + 1), 100 * (i + 1), 100, 100, Xwindow::Black);
             }
 
             // If square is occupied by piece, render text
@@ -28,8 +36,8 @@ void GraphicsObserver::notify() {
             if (!isWhite) {
                 int largeWidth = 100;   // width of the larger rectangle
                 int largeHeight = 100;  // height of the larger rectangle
-                int x = 100 * j;        // X coordinate of the larger rectangle's top-left corner
-                int y = 100 * i;        // Y coordinate of the larger rectangle's top-left corner
+                int x = 100 * (j + 1);  // X coordinate of the larger rectangle's top-left corner
+                int y = 100 * (i + 1);  // Y coordinate of the larger rectangle's top-left corner
 
                 // Calculate the center position of the larger rectangle
                 int centerX = x + (largeWidth / 2);
@@ -44,7 +52,7 @@ void GraphicsObserver::notify() {
 
             // Add a "w" or "b" to the piece to indicate color
             char color_indicator = _chess_board.lock()->getSquare(Position{i, j}).getPiece()->getColor() == Color::WHITE ? 'w' : 'b';
-            _w->drawString(100 * j + 50, 100 * i + 50, std::string{color_indicator} + std::string{cur});
+            _w->drawString(100 * (j + 1) + 50, 100 * (i + 1) + 50, std::string{color_indicator} + std::string{cur});
         }
     }
 }
